9fe16684d921d08eaf1480114bd1c625508cf25c
Xavier Caparros <xcaparros@hotmail.com>
2020-10-29 17:04:05 +0100

    'search-card'

--- project2/routes/search.js
+++ project2/routes/search.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const mtg = require('mtgsdk');
+const Card = require('../models/Card');
 var router = express.Router();
 
 router.get('/search', function(req, res, next) {
@@ -7,23 +7,42 @@ router.get('/search', function(req, res, next) {
 });
 
 router.post("/search", async (req, res, next) => {
-    // validamos los datos que vienen del formulario
-    let searchParams = {types:req.body.params};
-let search = async(searchParams)=>{
+const {name, set_name, rarity, legality, type, subtype, colors} = req.body;
+const paramsObj = {}
+if(name)paramsObj.name = new RegExp(name.trim(), 'i');
+if(set_name)paramsObj.set_name = new RegExp(set_name.trim(), 'i');
+if(rarity)paramsObj.set_name = new RegExp(rarity.trim(), 'i');
+if(legality)paramsObj[`legalities.${legality}`] = 'legal';
+if(colors){ //searches for the exact colors you put here, if you put red and blue a card with red blue and black would not appear
+  let colorsArr = colors.trim().split(',').join('').split(' ').map(color=>{
+    if(color === 'red' || color === 'Red' || color === 'RED')return 'R';
+    if(color === 'blue' || color === 'Blue' || color === 'BLUE')return 'U';
+    if(color === 'white' || color === 'White' || color === 'WHITE')return 'W';
+    if(color === 'green' || color === 'Green' || color === 'GREEN')return 'G';
+    if(color === 'black' || color === 'Black' || color === 'BLACK')return 'B';
+  }).sort();
+  paramsObj.colors = colorsArr;
+}
+if(type || subtype){
+  let regex = '';
+  let totalTypeArr = [];
+  if(type)totalTypeArr = [...type.trim().split(',').join('').split(' ')]; 
+  if(subtype)totalTypeArr = [...totalTypeArr, ...subtype.trim().split(',').join('').split(' ')];
+  totalTypeArr.forEach(el=>regex +=`(?=.*\\b${el}\\b)`);
+  let regex2 = new RegExp(regex+'.*', 'gi');
+    paramsObj.type_line = regex2;
+  }
+let search = async(paramsObj)=>{
         try{
-            const results = await mtg.card.where(searchParams);
+            const results = await Card.find(paramsObj);
             console.log(results);
         }catch(err){
             console.log(err);
         }
     }
-search(searchParams)
+search(paramsObj)
+
 res.redirect('/')
   });
 
 module.exports = router;
-
-//filters:
-//name: String, colors: array, types:array(creature, enchantment...), subtypes:array(vampire, elves...), rarity:string, setName:string, legalities[{format:'Comander', legality: 'Legal'},...]
-
-// let searchParams = {types:'Creature', subtypes:'elf,warrior'};
