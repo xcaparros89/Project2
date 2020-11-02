b2a5319091abc6d1146a4544bff8a1b748f6fe50
Xavier Caparros <xcaparros@hotmail.com>
2020-11-02 14:59:30 +0100

    deckValidator

--- project2/models/Deck.js
+++ project2/models/Deck.js
@@ -8,7 +8,9 @@ const deckSchema = new Schema(
     authorId: {type: String, require: true},
     mainCards: [{card: {type:Schema.Types.ObjectId, ref:'Card'}, count: Number}], // Min 60 No more than 4 of each excepting basic lands
     sideboard: [{card: {type:Schema.Types.ObjectId, ref:'Card'}, count: Number}],// Max 15
-    likes: {type: [String]},
+    legalities: {type: [String]},
+    colors:{type: [String], require: true},
+    likes: {type: [String], require: true},
     dislikes: {type: [String]},
     replies: {type: [{message:String, author:String}]}
   },
